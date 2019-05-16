42bb0241e37b36c5d1d1527ca81414ede7c49311
r00bal <hal9000@MacBook-Pro-r00bal.local>
2019-05-16 23:48:28 +0200

    add basic componets

---
+++ frontend/components/Meta.js
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+
+const Meta = () => (
+  <Head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+    <link rel="shortcut icon" href="/static/favicon.png" type="image/x-icon" />
+    <link rel="stylesheet" href="/static/nprogress.css" />
+    <title>Sick Fits!</title>
+  </Head>
+);
+
+export default Meta;
---
+++ frontend/pages/sell.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import Link from 'next/link';
+const Sell = props => (
+  <div>
+    <p>Sell!</p>
+    <Link href="/">Home!</Link>
+  </div>
+);
+
+export default Sell;
